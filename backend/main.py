f4f08b90559774c335e7907fcb221b782d85c18b
gptnikhil <gptnikhil500@gmail.com>
2024-08-31 11:53:09 +0530

    movie problem statement added

--- backend/main.py
+++ backend/main.py
@@ -3,4 +3,4 @@ import uvicorn
 
 
 if __name__=="__main__":
-    uvicorn.run("app.api:app",host="localhost",port=3000,reload=True)
+    uvicorn.run("app.api:app",host="localhost",port=9000,reload=True)
